83023aa13056dc1f4a82cd3d345a138d64e14ffa
lcrippa <crippa.lorenzo@gmail.com>
2025-04-25 09:25:14 +0200

    Updated format and documentation

--- src/edipy2/func_parse_umatrix.py
+++ src/edipy2/func_parse_umatrix.py
@@ -2,42 +2,60 @@ from ctypes import *
 import numpy as np
 import os, sys
 import types
-          
-            
+
+
 def reset_umatrix(self):
     """
 
     This function resets to 0 all the interaction coefficients
-    
+
     """
-    
+
     reset_umatrix_wrap = self.library.reset_umatrix
     reset_umatrix_wrap.argtypes = None
     reset_umatrix_wrap.restype = None
-    
+
     reset_umatrix_wrap()
-    
-    
-def add_twobody_operator(self,orbvector,spinvector,coefficient):
+
+
+def add_twobody_operator(self, oi, si, oj, sj, ok, sk, ol, sl, Uijkl):
     """
 
     This function lets the user add an interaction term on-the-fly.
-    The input parameters are of the form :code:`[o1,o2,o3,o4]` for the
-    orbital indices and :code:`[s1,s2,s3,s4]="u/d"` for the spin indices.
+    The input parameters are the spin and orbital indices of the second
+    quantized operators and the interaction coefficient.
     The order of the indices is consistent with those of the umatrix file
-    (see relevant EDIpack2.0 documentation).
-    
-    :type orbvector: int
-    :param orbvector: array of length 4, orbital indices
-    
-    :type orbvector: str
-    :param orbvector: array of length 4, spin indices
-    
-    :type float: coefficient
+    (see :ref:`EDIpack2.0 documentation <edipack2:parse_umatrix>`).
+
+    :type oi: int
+    :param oi: orbital index of :math:`c^{\dagger}_{i}`
+
+    :type si: str
+    :param si: spin index of :math:`c^{\dagger}_{i}`
+
+    :type oj: int
+    :param oj: orbital index of :math:`c^{\dagger}_{j}`
+
+    :type sj: str
+    :param sj: spin index of :math:`c^{\dagger}_{j}`
+
+    :type ok: int
+    :param ok: orbital index of :math:`c_{k}`
+
+    :type sk: str
+    :param sk: spin index of :math:`c_{k}`
+
+    :type ol: int
+    :param ol: orbital index of :math:`c_{l}`
+
+    :type sl: str
+    :param sl: spin index of :math:`c_{l}`
+
+    :type float: Uijkl
     :param orbvector: interaction coefficient
-    
+
     """
-    
+
     add_twobody_operator_wrap = self.library.add_twobody_operator
     add_twobody_operator_wrap.argtypes = [
         c_int,
@@ -48,12 +66,25 @@ def add_twobody_operator(self,orbvector,spinvector,coefficient):
         c_int,
         c_int,
         c_int,
-        c_double
+        c_double,
     ]
     add_twobody_operator_wrap.restype = None
-    
+
+    orbvector = [oi, oj, ok, ol]
+    spinvector = [si, sj, sk, sl]
+
     mapping = {"u": 1, "d": 2}
     spinvector = [mapping[x] for x in spinvector]
-    orbvector = [x+1 for x in orbvector]
-    
-    add_twobody_operator_wrap(orbvector[0],spinvector[0],orbvector[1],spinvector[1],orbvector[2],spinvector[2],orbvector[3],spinvector[3],coefficient)
+    orbvector = [x + 1 for x in orbvector]
+
+    add_twobody_operator_wrap(
+        orbvector[0],
+        spinvector[0],
+        orbvector[1],
+        spinvector[1],
+        orbvector[2],
+        spinvector[2],
+        orbvector[3],
+        spinvector[3],
+        Uijkl,
+    )
